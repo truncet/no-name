52e0b9f959d2300634f36b63f0460759070d8e2b
Sulove Bista <bistasulove@gmail.com>
2021-05-12 09:08:10 +0545

    Removed flask_script extension and fixed migration

--- manage.py
+++ manage.py
@@ -1,4 +1,3 @@
-from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 
 from models import db
@@ -6,9 +5,3 @@ from platform_service import server
 
 app = server.create_app() 
 migrate = Migrate(app, db)
-manager = Manager(app)
-manager.add_command('runserver', Server(host='localhost', port=5000, use_debugger=True))
-manager.add_command('db', MigrateCommand)
-
-if __name__ == '__main__':
-	manager.run()
